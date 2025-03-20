c688441e162d61af3f32f1021ae5000c40ff6d20
andrew.heavey <andrew.heavey@T606-19>
2025-03-21 12:06:22 +1300

    Add project files.

---
+++ BulkyRazorPages/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyRazorPages.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Please enter between 1 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ BulkyRazorPages/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using BulkyRazorPages.Data;
+using BulkyRazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyRazorPages.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+    }
+}
