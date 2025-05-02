10eaeacf304765fb45cb2c327e908a43b5953b1e
userHeaves <andrew.heavey@whitireianz.ac.nz>
2025-05-02 15:21:15 +1200

    Update

---
+++ BulkyRazorPages/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using BulkyRazorPages.Data;
+using BulkyRazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyRazorPages.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
